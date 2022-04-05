ab85a54bdbf0e69e3f86a94c58ebf869e4c2e0c7
LANGemmmmm <jiedong_lang@outlook.com>
2022-04-05 01:54:01 -0400

    first commit agin

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import helloController from "./controller/hello-controller.js";
 import userController from "./controller/user-controller.js";
 import tuitsController from "./controller/tuits-controller.js";
-// const express = require('express');gizzz
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -11,8 +11,5 @@ app.use(express.json());
 helloController(app);
 userController(app);
 tuitsController(app);
-// app.get('/hello',
-//     (req, res) => {res.send('Life is good!')});
-// app.get('/',
-//     (req, res) => {res.send('Welcome to Full Stack Development!')});
+
 app.listen(process.env.PORT || 4000);
