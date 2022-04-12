dcbdf9f2bf0e6902f451a940e8249eea282be543
LANGemmmmm <jiedong_lang@outlook.com>
2022-04-12 16:40:28 -0400

    update server again

--- server.js
+++ server.js
@@ -6,11 +6,8 @@ import tuitsController from "./controller/tuits-controller.js";
 import mongoose from "mongoose"
 
 
-//const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-// const CONNECTION_STRING = 'mongodb+srv://JOJOAPP:jojomongoapp@cluster0.4hfhh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/webdev'
 mongoose.connect(CONNECTION_STRING);
-// mongoose.connect('mongodb: //localhost:27017/webdev');
 
 const app = express();
 app.use(cors());
