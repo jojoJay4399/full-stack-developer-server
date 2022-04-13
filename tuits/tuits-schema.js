3300cd6ff5bca6284c2c9ab0bfe549f8adf1bdf9
LANGemmmmm <jiedong_lang@outlook.com>
2022-04-12 21:06:33 -0400

    update server again

--- tuits/tuits-schema.js
+++ tuits/tuits-schema.js
@@ -1,11 +1,21 @@
 import mongoose from 'mongoose';
 
 const schema = mongoose.Schema({
-    tuit: String,
-    likes: Number,
-    postedBy: {
-        username: String
-    }
+    tuit: {type: String, default: 'new Tuit'},
+    dislikes: {type : Number, default: 0},
+    likes: {type: Number, default: 0},
+    username: {type: String, default: 'JOJO'},
+    handle : {type: String, default: 'jojo'},
+    time: {type: String, default: 'a second ago'},
+    withTuit: {type : Boolean, default: true},
+    image: {type : String, default: '../../../images/bookmarks1.jpg'},
+    avatar: {type : String, default: '../../../images/3.jpg'},
+    liked: Boolean,
+    disliked: Boolean
+    // postedBy: {
+    //     username: String
+    // }
+
 }, {collection: 'tuits'});
 export default schema;
 
